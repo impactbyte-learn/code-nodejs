6836bc5ee2b2aea40eb8691fc788ef7a78f4f9b9
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-01 11:53:19 +0700

    Refactor data files location

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const colors = require("colors");
 
-const PEOPLE = require("./data_people.js");
-const SAIYANS = require("./data_saiyans.js");
+const PEOPLE = require("./data/people.js");
+const SAIYANS = require("./data/saiyans.js");
 
 const displayContacts = DATA => {
 	const MAPPED_DATA = DATA.map(({ name, phoneNumber }) => {
