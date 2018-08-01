5c75e5a460eaf2abf53dc3fe7832d499bdb3af09
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-01 14:40:35 +0700

    Optimize main app

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const colors = require("colors");
-
-const PEOPLE = require("./data/people.js");
-const SAIYANS = require("./data/saiyans.js");
+const PEOPLE = require("../data/people.js");
+const SAIYANS = require("../data/saiyans.js");
 
 const displayContacts = DATA => {
 	const MAPPED_DATA = DATA.map(({ name, phoneNumber }) => {
