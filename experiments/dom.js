e7bbc8ba3fe14f03f1c870297b706cd1da7fca9a
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-01 14:41:11 +0700

    Add DOM example using cheerio

---
+++ experiments/dom.js
@@ -0,0 +1,9 @@
+const cheerio = require("cheerio");
+const $ = cheerio.load('<h2 class="title">Hello world</h2>');
+
+$("h2.title").text("Hello there!");
+$("h2").addClass("welcome");
+
+const html = $.html();
+
+console.log(html);
